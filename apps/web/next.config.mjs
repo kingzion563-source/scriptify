b72b479bc9fa8a13b02ad8bf42ee3ebe9d49661a
zionking <zionking@zionkings-MacBook-Pro.local>
2026-03-07 16:40:42 -0500

    remove sentry wrapper

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -41,9 +41,4 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: true,
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  disableLogger: true,
-});
+export default nextConfig;
