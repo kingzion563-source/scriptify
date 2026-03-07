6a83a27cfc26482a97e762ec292da1c9cf8c7fdd
zionking <zionking@zionkings-MacBook-Pro.local>
2026-03-07 16:51:02 -0500

    remove custom 500 page

--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "53dd01c346065b83"; }
+/******/ 		__webpack_require__.h = function() { return "d82b4b2c95abd79f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
